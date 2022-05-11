c807118985dca95044eb0611158fd587a1377112
feizheng <1290657123@qq.com>
2022-05-11 09:49:17 +0800

    wip

--- src/2022-05/03-spy/01.spy.spec.js
+++ src/2022-05/03-spy/01.spy.spec.js
@@ -7,6 +7,8 @@ test('should read the file', () => {
 
   const result = fs.readFileSync('./01.spy');
   expect(result).toBe('Hello World');
+  expect(fs.readFileSync.mock.calls.length).toBe(1);
+  expect(fs.readFileSync.mock.calls[0][0]).toBe('./01.spy');
 });
 
 //npx jest ./src/2022-05/03-spy/01.spy.spec.js
